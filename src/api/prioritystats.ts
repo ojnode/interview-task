a2d1d9010827d01cb679835ab6b28425a0742901
ojnode <ojinnakabu95@gmail.com>
2024-12-11 21:38:31 +0000

    add API endpoint to calculate priority percentages

---
+++ src/api/prioritystats.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Request, Response } from 'express';
+import { SampleData } from './types';
+
+const DATA_URL = 'https://sampleapi.squaredup.com/integrations/v1/service-desk?datapoints=500';
+
+// function to calculate percetange of individual types
+function typePercentage(results: { priority: String}[], priority: string ): number {
+    let count = 0;
+
+    for (let i = 0; i < results.length; i++) {
+        if (results[i].priority == priority) {
+            count++;
+        }
+    }
+
+    const percentage = ( count / results.length) * 100;
+    return Number(percentage.toFixed(2));
+}
+
+export const GET = async (req: Request, res: Response) => {
+    const { data } = await axios.get<SampleData>(DATA_URL)
+    const high = typePercentage(data.results, "high")
+    const medium = typePercentage(data.results, "medium")
+    const low = typePercentage(data.results, "low")
+    res.json({ "Priority percentages":  
+        { high, medium, low }
+     });
+};
